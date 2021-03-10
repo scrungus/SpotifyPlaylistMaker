284cd33645aefb34a36c719a0ec56a5c9b431b98
scrungus <scrungus@riseup.net>
2021-03-10 18:02:06 +0000

    added headers

--- frontend/src/hooks/spotifyAPI.ts
+++ frontend/src/hooks/spotifyAPI.ts
@@ -4,6 +4,8 @@ export function sendGetRequest() {
   const http = new XMLHttpRequest();
   const url = "http://spotifyplaylistmaker_auth_1:8000/login";
   http.open("GET", url);
+  http.setRequestHeader("Access-Control-Allow-Origin", "*");
+  http.setRequestHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   http.send();
   
   http.onreadystatechange = (e) => {
