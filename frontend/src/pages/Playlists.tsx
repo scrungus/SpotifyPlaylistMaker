b44923c77c229f22e1460fb38e247bd76cc80c60
Samuel Guard <sg2268@bath.ac.uk>
2021-03-15 16:14:24 +0000

    finally got async request working

--- frontend/src/pages/Playlists.tsx
+++ frontend/src/pages/Playlists.tsx
@@ -1,20 +1,98 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonHeader, IonItem, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import PlaylistBox from '../components/PlaylistContainer';
 import './Playlists.css';
+import React, { useEffect } from 'react';
+import { contrastOutline } from 'ionicons/icons';
 
-const Playlists: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Playlists</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <ExploreContainer name="Tab 2 page" />
-      </IonContent>
-    </IonPage>
-  );
-};
+interface Playlist {
+  name: string
+  link: string
+  icon: null
+}
+
+async function getPlaylists(userID: number) {
+  const http = new XMLHttpRequest();
+  //let url = "http://" + process.env.REACT_APP_DB_URL + `/getUserPlaylists?id=${userID}`;
+  let url = `http://localhost:8002/getUserPlaylists?id=${userID}`;
+  http.open("GET", url);
+  http.send()
+
+  let prom = new Promise((resolve, reject) => {
+    http.onload = () => {
+      console.log(http.response);
+      resolve(http.response);
+    }
+  });
+
+  return prom;
+}
+
+type PlaylistsState = {
+  playlists: any;
+  userID: number;
+}
+
+class Playlists extends React.Component<{}, PlaylistsState> {
+  constructor(props: Playlist){
+    super(props)
+  }
+  componentWillMount(){
+    this.setState({playlists: [], userID: 18});
+  }
+  componentDidMount() {
+    getPlaylists(this.state.userID).then((data: any) => {
+      data = JSON.parse(data);
+      this.setState({playlists: (data.data), userID: 18});
+    });
+  }
+
+
+
+  render() {
+    let userID = 18;
+
+
+    //let playlists = [{name: "p1", link: "http://www.google.com", icon: null}, {name: "p2", link: "http://www.spotify.com", icon: null}];
+
+
+    //let playlists = getPlaylists(userID);
+
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Playlists</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent fullscreen>
+          <IonList>
+            {
+              this.state.playlists.map((play: any, index: number) => {
+                return (
+                  <IonItem key={index}>
+                    <PlaylistBox name={"name"} link={play[1]} icon={"icon"}></PlaylistBox>
+                  </IonItem>
+                )
+              }
+              )
+            }
+          </IonList>
+        </IonContent>
+      </IonPage>
+    );
+  }
+}
+
+/*
+{
+            {
+        playlists: [
+          { name: "p1", link: "http://www.google.com", icon: null },
+          { name: "p2", link: "http://www.spotify.com", icon: null }
+        ]
+      }
+          } 
+          */
+
 
 export default Playlists;
