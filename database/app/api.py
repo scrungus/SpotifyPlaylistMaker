6536f98f6b86e9f3602a69a2d3224144185d6e91
scrungus <scrungus@riseup.net>
2021-03-21 17:44:05 +0000

    caught index err on no profile pic

--- database/app/api.py
+++ database/app/api.py
@@ -79,8 +79,10 @@ class addUserRequest(BaseModel):
 
 @app.post("/addUser",tags=["addUser"])
 async def addUser(req : Dict[Any,Any]):
-    print("new user added!")
-    return db.addUser(req['display_name'], req['id'], req['access_token'],req['images'][0]['url'])
+    if(len(req['images'] > 0)):
+        return db.addUser(req['display_name'], req['id'], req['access_token'],req['images'][0]['url'])
+    else:
+        return db.addUser(req['display_name'], req['id'], req['access_token'])
 
 class addPlaylistRequest(BaseModel):
     link: str
